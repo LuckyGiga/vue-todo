9bd2730df3a29abd88dd30906b93ddd987aeeab0
klukiyan <kiril.lukiyan@gmail.com>
2019-08-27 23:02:14 +0200

    Up to header addition

---
+++ src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <Todo :todo="todo"  v-on:del-todo="$emit('del-todo', todo.id)"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Todo'
+export default {
+  name: "Todos",
+  props: ["todos"],
+  components:{
+    Todo,
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="home">
+      <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Todos from '@/components/Todos';
 
 export default {
   name: 'home',
   components: {
-    
-  }
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title : "First Todo",
+          completed: false
+        },
+        {
+          id: 2,
+          title : "Second Todo",
+          completed: true
+        },
+        {
+          id: 3,
+          title : "Third Todo",
+          completed: false
+        },
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(item=>item.id !== id);
+    }
+  },
 }
 </script>
