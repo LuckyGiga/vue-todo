4fa9abbf7bd680fc7ed2f0eaa797785e1f927679
klukiyan <kiril.lukiyan@gmail.com>
2019-08-30 20:16:59 +0200

    About work

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>Quick vue application that works offline, is compatible with Android & iPhone as progressive Web App.</p>
+    <p>Can be used for Grossery list or quick todos</p>
   </div>
 </template>
